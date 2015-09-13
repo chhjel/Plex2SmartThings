5757623676d5dae4cf27805039defbd2baa2eca9
Christian H <ch.hjel@gmail.com>
2015-09-13 11:35:58 +0200

    Initial commit.

---
+++ PlexWatch2SmartThings/Plex2SmartThings/UserStateManager.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Plex2SmartThings
+{
+    /// <summary>
+    /// Manages a list of the current states per player
+    /// </summary>
+    class UserStateManager
+    {
+        public enum PlayStates { UNKNOWN = 0, PLAY = 1, PAUSE = 2, STOP = 3 }
+        private List<InstancePlayState> PlayerStates = new List<InstancePlayState>();
+        public List<string> ActivePlayerNames = new List<string>();
+
+        public void ParsePlexResult(string rawXml)
+        {
+            ActivePlayerNames.Clear();
+
+            //Loop through plex results
+            try { 
+                XDocument doc = null;
+                doc = XDocument.Parse(rawXml);
+                XElement e = doc.XPathSelectElement("/MediaContainer");
+                foreach (XElement video in e.Elements())
+                {
+                    ParseVideoElement(video);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to parse xml from Plex: " + ex.Message);
+                return;
+            }
+
+            //Now check for any expired events = stopped
+            for (int i = 0; i < PlayerStates.Count; i++)
+            {
+                if (!ActivePlayerNames.Contains(PlayerStates[i].PlayerName))
+                {
+                    PlayerStates[i].OnStateRetrieved(PlayStates.STOP);
+                    if (PlayerStates[i].RequestListRemoval)
+                    {
+                        PlayerStates[i].AbortThreads = true;
+                        PlayerStates.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        private void ParseVideoElement(XElement video)
+        {
+            //Parse the raw data into a InstancePlayState object
+            UserStateManager.InstancePlayState playerData = new UserStateManager.InstancePlayState();
+            if (!playerData.Parse(video)) return;
+
+            //Notify state update to existing data if any
+            bool isInList = false;
+            for (int i = 0; i < PlayerStates.Count; i++) {
+                if (PlayerStates[i].PlayerName == playerData.PlayerName) {
+                    isInList = true;
+                    PlayerStates[i].OnStateRetrieved(playerData.CurrentState);
+                    break;
+                }
+            }
+
+            //No existing player with that name => add it to list
+            if (!isInList)
+            {
+                PlayerStates.Add(playerData);
+                //Forced state change after we just added it, since the state is the same as when it was parsed
+                playerData.OnStateRetrieved(playerData.CurrentState, true);
+            }
+
+            //Add player to the active list
+            ActivePlayerNames.Add(playerData.PlayerName);
+        }
+
+        public class InstancePlayState
+        {
+            public PlayStates CurrentState { get; set; }
+            public string PlayerName { get; set; }
+            public string UserName { get; set; }
+            public string Type { get; set; } //Movie, episode etc. Whatever plex puts in the /Video/@type attribute
+
+            public bool RequestListRemoval { get; private set; }
+
+            /// <summary>
+            /// Parses the raw data into this object.
+            /// </summary>
+            /// <param name="raw"></param>
+            /// <returns>true if the parse was a success.</returns>
+            public bool Parse(XElement video)
+            {
+                //Parse values
+                UserName = video.Element("User").Attribute("title").Value;
+                PlayerName = video.Element("Player").Attribute("title").Value;
+                Type = video.Attribute("type").Value;
+
+                if (!CanProcess(UserName, PlayerName, Type)) return false;
+
+                //Parse state
+                string stateStr = video.Element("Player").Attribute("state").Value;
+                CurrentState = PlayStates.UNKNOWN;
+                if (stateStr == "playing")
+                {
+                    CurrentState = PlayStates.PLAY;
+                }
+                else if (stateStr == "paused")
+                {
+                    CurrentState = PlayStates.PAUSE;
+                }
+
+                return true;
+            }
+
+            /// <summary>
+            /// Checks if the state has changed and notifies the smartthings endpoint.
+            /// </summary>
+            /// <param name="state"></param>
+            /// <param name="forced">Overrides the check vs previous state</param>
+            public void OnStateRetrieved(PlayStates state, bool forced=false)
+            {
+                if (Program.DebugLevel >= 2) Console.WriteLine(UserName + "@'" + PlayerName + "' "+Type+"=> StateRetrieved(" + state.ToString() + ")");
+
+                //Something weird has happened.. => abort
+                if (state == PlayStates.UNKNOWN) return;
+                //No new state => abort
+                else if (state == CurrentState && !forced) return;
+                //We got a new state
+                else CurrentState = state;
+
+                if (Program.DebugLevel >= 1) Console.WriteLine(UserName + "@'" + PlayerName + "' Changed state to: " + state.ToString());
+
+                //Select the correct endpoint
+                Delay = Config.GetDelayFor(CurrentState, Type);
+
+                if (Delay > 0)
+                {
+                    if (Program.DebugLevel >= 1) Console.WriteLine(" >Delay: " + Delay);
+                }
+                else
+                {
+                    string endpoint = CreateEndpointUrl();
+                    Program.SendGetRequest(endpoint);
+
+                    if (CurrentState == PlayStates.STOP) RequestListRemoval = true;
+                }
+                
+                if (stateDelayThread == null)
+                {
+                    stateDelayThread = new Thread(() => ProcessStateDelay());
+                    stateDelayThread.Start();
+                }
+            }
+
+            private Thread stateDelayThread;
+            private int Delay;
+            public bool AbortThreads = false;
+            private void ProcessStateDelay()
+            {
+                while (!AbortThreads && !Program.Terminate)
+                {
+                    if (Delay >= 1)
+                    {
+                        Delay--;
+                        if (Delay <= 0)
+                        {
+                            string endpoint = CreateEndpointUrl();
+                            if (Program.DebugLevel >= 1) Console.WriteLine("Sent delayed state change: " + CurrentState.ToString());
+                            Program.SendGetRequest(endpoint);
+
+                            if (CurrentState == PlayStates.STOP) RequestListRemoval = true;
+                        }
+                    }
+
+                    Thread.Sleep(1000);
+                    if (Program.Terminate || AbortThreads) return;
+                }
+            }
+
+            private string CreateEndpointUrl()
+            {
+                string endpoint = "";
+                if (CurrentState == PlayStates.PLAY) endpoint = Config.EndpointUrl_OnPlay;
+                else if (CurrentState == PlayStates.PAUSE) endpoint = Config.EndpointUrl_OnPause;
+                else if (CurrentState == PlayStates.STOP) endpoint = Config.EndpointUrl_OnStop;
+
+                //Prepare for first param, just in case there is already added some params in the url in the config file
+                if (!endpoint.Contains("?")) endpoint += "?";
+                else endpoint += "&";
+
+                //Add the GET parameters to the request
+                endpoint += "access_token=" + HttpUtility.UrlEncode(Config.Endpoint_AccessToken);
+                endpoint += "&player=" + HttpUtility.UrlEncode(PlayerName);
+                endpoint += "&user=" + HttpUtility.UrlEncode(UserName);
+                endpoint += "&type=" + HttpUtility.UrlEncode(Type);
+
+                return endpoint;
+            }
+
+            /// <summary>
+            /// Check that values are not blank, and check vs ignore/whitelists.
+            /// </summary>
+            /// <param name="user"></param>
+            /// <param name="player"></param>
+            /// <returns></returns>
+            private bool CanProcess(string user, string player, string type)
+            {
+                //Both blank => not allowed
+                if (String.IsNullOrEmpty(user) && String.IsNullOrEmpty(player)) return false;
+
+                //Whitelist have top priority
+                if (Config.WhiteListContains("user", user)) return true;
+                else if (Config.WhiteListContains("player", player)) return true;
+                else if (Config.WhiteListContains("type", type)) return true;
+
+                if (Config.OnlyAllowWhitelisted) return false;
+
+                //Check ignore list
+                if (Config.IgnoreListContains("user", user)) return false;
+                else if (Config.IgnoreListContains("player", player)) return false;
+                else if (Config.IgnoreListContains("type", type)) return false;
+
+                //Not on whitelist or ignorelist, and OnlyAllowWhitelisted is false
+                return true;
+            }
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Plex2SmartThings
-Integration from Plex to SmartThings, notifying a smartapp when media starts playing on Plex.
+Integration from Plex to SmartThings, notifying a smartapp when media starts playing (or pauses/stops) on Plex.
+
+## Requirements
+
+- [SmartThings](http://www.smartthings.com/) home automation system.
+- [Plex Media Server](https://plex.tv/)
+
+## How to install it
+
+### 1. Create the SmartApp
+
+1. Go to https://graph.api.smartthings.com/ide/apps and create a new SmartApp.
+2. Fill out the required fields, and at the bottom of the app creation page be sure to enable OAuth by clicking the button "Enable OAuth in the Smart App".
+3. Note down the OAuth client id and secret. You will need both of them for later.
+4. Replace the apps generated source code with the code from the file SmartApp_Source.groovy.txt.
+5. Save and publish the app for yourself.
+6. Configure the app and note down the API Token and API Endpoint values.
+![](https://lh6.googleusercontent.com/IkYz19RC2T47L9kIaROifhE9-U1qY1dUKfvIpfSSZmph8kW-UAYnhDA_3TcYKXZ74PuCu8fqAAjusHkDFoxNSjSscsoFL2QPYJTGIh4UUNOLh6_vJzxY3kU9mCc8qid4VaVoXXk?raw=true)
+
+### 2. Configure the Plex2SmartThings application
+
+1. Compile the Plex2SmartThings application.
+2. Open the config.config file.
+3. In config/smartThingsEndpoints fill in your API token and endpoint urls. Be sure to keep the /statechanged/on* at the end of the urls.
+4. If plex and this application is not running on the same server then enter the url to the session status page of your plex server in the config/plexCheck/@plexStatusUrl attribute.
+5. Configure the rest of the file to your likings.
+
+### 3. Run Plex2SmartThings
+
+You can now run Plex2SmartThings and the SmartApp should be notified whenever any media plays on plex.
+
+If anything isn’t working you can try enabling some extra debug output by adding the d1 or d2 arguments. E.g. running Plex2SmartThings.exe d1 from the command line.
+
